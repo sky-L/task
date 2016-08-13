d8ff3ed41ac204f809217d582cb3e275c48dd405
sky_l <690035384@qq.com>
2016-08-13 09:59:27 +0800

    init  tieba

--- action/SpiderAction.php
+++ action/SpiderAction.php
@@ -12,7 +12,6 @@ require "BaseAction.php";
 use Symfony\Component\DomCrawler\Crawler;
 use Symfony\Component\CssSelector;
 use follow;
-
 use tieba;
 
 class SpiderAction extends BaseAction
@@ -29,79 +28,172 @@ class SpiderAction extends BaseAction
     public function __construct()
     {
         $this->withCapsule("tieba");
+    
+       // $this->init_tieba();
         
-        $keys = tieba::where(['tieba_status' => 1])->skip(0)->take(10)->get()->toArray();
+        $keys = tieba::where([
+            'tieba_status' => 1
+        ])->skip(0)
+            ->take(1)
+            ->get()
+            ->toArray();
         
-        foreach ($keys as $k => $v)
+        if ($keys)
         {
-            tieba::where(['id' => $v['id']])->update(['tieba_status' => 2]);
-        }
-        
-        foreach ($keys as $id => $t_name)
-        {
-            $name = $this->record_follow_tieba($t_name['name']);
-            
-            foreach ($name as $k => $v)
+            foreach ($keys as $k => $v)
             {
-                $this->detail($v,$k,$t_name['name']);
+                tieba::where([
+                    'id' => $v['id']
+                ])->update([
+                    'tieba_status' => 2
+                ]);
+            }
+            
+            foreach ($keys as $id => $t_name)
+            {
+                $name = $this->record_follow_tieba($t_name['name']);
+                
+                $l = count($name);
+                
+                echo "-fetching detail\n";
+                
+                foreach ($name as $k => $v)
+                {
+                    $this->detail($v, $k, $t_name['name'],$l);
+                }
             }
         }
-        
     }
 
-    public function detail($name = "",$i = "" , $tieba = "")
+    
+    public function init_tieba()
     {
-        $url = "http://www.baidu.com/p/".$name."/detail";
+        
+        $url = "http://tieba.baidu.com/f/like/mylike?&pn=1";
+        
+        $html = $this->request("get", $url);
+        
+        $html = mb_convert_encoding($html, "UTF-8", "gbk,gb2312,UTF-8");
+       
+        echo "--geting html---\n";
+        
+        preg_match('/\<a href="\/f\/like\/mylike\?\&pn=(.*?)"\>尾页\<\/a\>/', $html, $total);
+        
+        $total = $total[1];
+        
+        $names = [];
+        
+        echo "--geting title---\n";
+        
+        for($i = 1; $i <= $total; $i++)
+        {
+            $url = "http://tieba.baidu.com/f/like/mylike?&pn=" . $i; 
+            
+            $names = array_merge($names,$this->fetch_tieba($url));
+        }
+       
+        echo "--insert database---\n";
+        
+        foreach ($names as $k => $v)
+        {
+            if(!tieba::where(['name' => $v])->get()->first())
+            {
+                \tieba::create(['name' => $v]);
+            }
+        }
+     
+        echo "-- down insert ---\n";
+        
+        exit(__FILE__ . __LINE__);
+    }
+    
+    public function fetch_tieba($url)
+    {
+        $html = $this->request("get", $url);
+        
+        $html = mb_convert_encoding($html, "UTF-8", "gbk,gb2312,UTF-8");
+         
+        $crawler = $this->craw($html);
+        
+        $found = $crawler->filter("table tr");
+        
+        if ($found->count())
+        {
+            $data = $found->each(function (Crawler $node, $i)
+            {
+                if($node->filter("a")->count())
+                {
+                    return $node->filter("a")->eq(0)->attr("title");
+                }
+                 
+                return  false;
+            });
+        
+            $data = array_filter($data);
+        }
+        
+        return $data;
+    }
+    
+    public function detail($name = "", $i = "", $tieba = "",$count = "")
+    {
+        $url = "http://www.baidu.com/p/" . $name . "/detail";
         
         $content = $this->request("get", $url);
         
         $content = mb_convert_encoding($content, "UTF-8", "gbk,gb2312,UTF-8");
         
-        preg_match('/\<span class=profile-attr\>个人简介\<\/span\>\s+\<span class=profile-cnt\>(.*?)\<\/span\>/', $content,$detail);
-         
-        if($detail && $profile =  return_mobile($detail[1]))
+        preg_match('/\<span class=profile-attr\>个人简介\<\/span\>\s+\<span class=profile-cnt\>(.*?)\<\/span\>/', $content, $detail);
+        
+        if ($detail && $profile = return_mobile($detail[1]))
         {
-            follow::create(['name' => $name , "profile" => $profile,"tieba" => $tieba]);
+            follow::create([
+                'name' => $name,
+                "profile" => $profile,
+                "tieba" => $tieba
+            ]);
         }
-         
-        echo $i . "down_\n";
+        
+        echo $tieba . "-" .$count ."--". $i . "--down\n";
     }
 
-    
     /**
      * 获取所有楼主
-     * @param string $key 贴吧名称
+     * 
+     * @param string $key
+     *            贴吧名称
      * @return multitype:
      */
     public function record_follow_tieba($key = "中国文玩核桃")
     {
-        $content = $this->request("get", "http://tieba.baidu.com/f?kw=".$key."&ie=utf-8&pn=0");
+        $content = $this->request("get", "http://tieba.baidu.com/f?kw=" . $key . "&ie=utf-8&pn=0");
         
         $content = mb_convert_encoding($content, "UTF-8", "gbk,gb2312,UTF-8");
         
-        preg_match_all('/\<span class="red_text"\>(.*?)\<\/span\>/', $content,$total);
+        preg_match_all('/\<span class="red_text"\>(.*?)\<\/span\>/', $content, $total);
         
         $total = $total[1][0];
         
         $l = ceil($total / 50);
         
         $name = [];
-     
-        for($i = 0; $i<$l; $i++)
+        
+        echo "-geting tieba name\n";
+        
+        for ($i = 0; $i < $l; $i ++)
         {
-            $url = "http://tieba.baidu.com/f?kw=".$key."&ie=utf-8&pn=" . $i * 50;
+            $url = "http://tieba.baidu.com/f?kw=" . $key . "&ie=utf-8&pn=" . $i * 50;
             
-            $name = array_merge($name,$this->return_name($url));
-            
+            $name = array_merge($name, $this->return_name($url));
         }
         
         return $name;
     }
 
-    
     /**
      * 发帖人用户名
-     * @param unknown $url
+     * 
+     * @param unknown $url            
      * @return unknown
      */
     public function return_name($url)
@@ -110,11 +202,11 @@ class SpiderAction extends BaseAction
         
         $content = mb_convert_encoding($content, "UTF-8", "gbk,gb2312,UTF-8");
         
-        preg_match_all('/"主题作者:\s+(.*?)"/', $content,$names);
+        preg_match_all('/"主题作者:\s+(.*?)"/', $content, $names);
         
         return $names[1];
     }
-    
+
     public static function getCookie()
     {
         $cookie = '';
